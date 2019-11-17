41b4d780e91a4841ace671648fa78eecf3188f00
javierrodriguezregueiro <javiro238@gmail.com>
2019-11-17 13:52:46 +0100

    Added websocket to chat component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Chat from "./Components/Chat/Chat";
 
--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -3,8 +3,8 @@ import Conversations from "../Conversations/Conversations";
 import "./Chat.css";
 import Messages from "../Messages/Messages";
 
-const Convers = [{name: 'Juan', id: 1}, {name: 'Pepe', id: 2}, {name: 'Neppe', id: 3}, {name: 'Juan', id: 1}, {name: 'Pepe', id: 2}, {name: 'Neppe', id: 3}, {name: 'Juan', id: 1}, {name: 'Pepe', id: 2}, {name: 'Neppe', id: 3}, {name: 'Juan', id: 1}, {name: 'Pepe', id: 2}, {name: 'Neppe', id: 3}, {name: 'Juan', id: 1}, {name: 'Pepe', id: 2}, {name: 'Neppe', id: 3}];
-
+const Convers = [{name: 'Juan', id: 1}, {name: 'Pepe', id: 2}, {name: 'Neppe', id: 3}, {name: 'Juan', id: 4}, {name: 'Pepe', id: 5}, {name: 'Neppe', id: 6}, {name: 'Juan', id: 7}, {name: 'Pepe', id: 8}, {name: 'Neppe', id: 9}, {name: 'Juan', id: 10}, {name: 'Pepe', id: 11}, {name: 'Neppe', id: 12}, {name: 'Juan', id: 13}, {name: 'Pepe', id: 14}, {name: 'Neppe', id: 15}];
+let socket = new WebSocket("ws://192.168.1.18:8080");
 class Chat extends React.Component {
     constructor(props) {
         super(props);
@@ -12,6 +12,15 @@ class Chat extends React.Component {
             selected : 0,
             label: 'Mariano'
         }
+    }
+
+    componentDidMount() {
+        socket.onopen = () => {
+            console.log('Websocket connected!');
+        };
+        socket.onmessage = (message) => {
+            console.log(message);
+        };
     }
 
     setChatState = (labelValue, selectedValue) => {
