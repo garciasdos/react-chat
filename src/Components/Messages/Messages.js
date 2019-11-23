1a3dfd0a4ca9ebd4bca9a2bd6ae38d2fe66844ac
javierrodriguezregueiro <javierrodriguezregueiro@gmail.com>
2019-11-23 13:29:08 +0100

    Refactored messageFooter component to work properly with message to send

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -4,14 +4,42 @@ import MessagesHeader from "./MessagesHeader/MessagesHeader";
 import MessagesFooter from "./MessagesFooter/MessagesFooter";
 import MessagesBody from "./MessagesBody/MessagesBody";
 
-const Message = (props) => {
-    return (
-        <section className='messages'>
-            <MessagesHeader label={props.label}/>
-            <MessagesBody/>
-            <MessagesFooter/>
-        </section>
-    );
+class Message extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            messageToSend: ''
+        }
+    };
+
+    setMessageToSend = (value) => {
+        this.setState({
+            messageToSend: value
+        })
+    };
+
+    sendData = () => {
+        //TODO: this must send data throw websocket
+        console.log(this.state.messageToSend);
+    };
+
+    resetMessage = () => {
+        this.setState({
+            messageToSend: ''
+        })
+    };
+
+    render() {
+        return (
+            <section className='messages'>
+                <MessagesHeader label={this.props.label}/>
+                <MessagesBody/>
+                <MessagesFooter messageToSend={this.state.messageToSend} onChange={this.setMessageToSend}
+                                sendData={this.sendData} resetValue={this.resetMessage}/>
+            </section>
+        );
+    }
 };
 
 export default Message;
--- src/Components/Messages/MessagesFooter/MessagesFooter.js
+++ src/Components/Messages/MessagesFooter/MessagesFooter.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import "./MessagesFooter.css";
 
-const MessagesFooter = () => {
-    return (
-        <footer className='messagesFooter'>
-            <input type="text"/>
-        </footer>
-    );
-};
+class MessagesFooter extends React.Component {
+
+    execSendData = () => {
+        this.props.sendData();
+        this.props.resetValue();
+    };
+    onPressButton = () => {
+        this.execSendData();
+    };
+
+    onChangeInput = (e) => {
+        this.props.onChange(e.target.value);
+    };
+
+    onKeyDownInput = (e) => {
+        if (e.keyCode === 13 && this.props.messageToSend !== '') {
+            this.execSendData()
+        }
+    };
+
+    render() {
+        return (
+            <footer className='messagesFooter'>
+                <input type='text' value={this.props.messageToSend} placeholder='Type a message'
+                       className='messagesFooter-input'
+                       onChange={this.onChangeInput} onKeyDown={this.onKeyDownInput}/>
+                <button className='messagesFooter-button' onClick={this.onPressButton}></button>
+            </footer>
+        );
+    };
+}
 
 export default MessagesFooter;
